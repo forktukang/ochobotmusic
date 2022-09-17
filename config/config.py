fdd106fd6226128f22710aae671200a0682fd70b
forktukang <102500858+forktukang@users.noreply.github.com>
2022-09-17 22:49:03 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -59,7 +59,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", None
+    "SUPPORT_CHANNEL", https://t.me/ReyzuSupport
 )  # Example:- https://t.me/ReyzuSupport
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", None
@@ -94,7 +94,7 @@ YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "3"))
 TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/Reyzuuu/ReyzuMusic")
+GITHUB_REPO = getenv("GITHUB_REPO", None)
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
