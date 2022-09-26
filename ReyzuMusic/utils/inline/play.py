62ce3d404dcf3e26e4bafce0e5ea63dc8c836463
forktukang <102500858+forktukang@users.noreply.github.com>
2022-09-26 22:14:28 +0700

    Update play.py

--- ReyzuMusic/utils/inline/play.py
+++ ReyzuMusic/utils/inline/play.py
@@ -111,7 +111,7 @@ def stream_markup(_, videoid, chat_id):
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
             ),
         ],
         [
