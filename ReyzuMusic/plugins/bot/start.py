a18e551179c5b43614eb195ab29396aca421755b
forktukang <102500858+forktukang@users.noreply.github.com>
2022-09-29 18:41:57 +0700

    Update start.py

--- ReyzuMusic/plugins/bot/start.py
+++ ReyzuMusic/plugins/bot/start.py
@@ -41,6 +41,7 @@ async def start_comm(client, message: Message, _):
             keyboard = help_pannel(_)
             return await message.reply_text(
                 _["help_1"].format(config.MUSIC_BOT_NAME),
+                reply_markup=keyboard,
             )
         if name[0:4] == "song":
             return await message.reply_text(_["song_2"])
