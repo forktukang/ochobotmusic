4bd19e923c1c7ced05c6f829a32908b5066528d9
forktukang <102500858+forktukang@users.noreply.github.com>
2022-09-30 15:48:36 +0700

    Update help.py

--- ReyzuMusic/utils/inline/help.py
+++ ReyzuMusic/utils/inline/help.py
@@ -17,7 +17,7 @@ def help_pannel(_, START: Union[bool, int] = None):
             callback_data=f"settingsback_helper",
         ),
         InlineKeyboardButton(
-            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+            text=_["CLOSE_BUTTON"], callback_data=f"close"
         ),
     ]
     mark = second if START else first
