3fa52ea4df8ae6efc505405f9d92b4905c6b2980
forktukang <102500858+forktukang@users.noreply.github.com>
2022-11-10 14:06:21 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -112,7 +112,7 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "60"))
 
 # Cleanmode time after which bot will delete its old messages from chats
 CLEANMODE_DELETE_MINS = int(
-    getenv("CLEANMODE_MINS", "5")
+    getenv("CLEANMODE_MINS", "1440")
 )  # Remember to give value in Seconds
 
 
