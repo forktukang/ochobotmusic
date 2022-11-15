0409f88fc6072671626afa3551bc15bd57f5aedb
forktukang <102500858+forktukang@users.noreply.github.com>
2022-11-15 08:23:38 +0700

    Update autoleave.py

--- ReyzuMusic/plugins/misc/autoleave.py
+++ ReyzuMusic/plugins/misc/autoleave.py
@@ -70,7 +70,7 @@ async def auto_end():
                 try:
                     await app.send_message(
                         chat_id,
-                        "Tidak ada aktivitas di obrolan. Streaming telah dihentikan.",
+                        "Tidak ada aktivitas di obrolan. Bot telah dihentikan untuk menghindari kelebihan beban di server.",
                     )
                 except:
                     continue
